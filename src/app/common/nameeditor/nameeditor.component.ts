cb9aa6a501f6dc1c4cac32c01ae5944d5f025bdd
Suhas-lab <suhas.korhale@github.com>
2020-07-04 16:29:13 +0530

    name editor component updated

--- src/app/common/nameeditor/nameeditor.component.ts
+++ src/app/common/nameeditor/nameeditor.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormControl, FormGroup } from '@angular/forms';
+import { FormControl, FormGroup, FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-nameeditor',
@@ -8,20 +8,50 @@ import { FormControl, FormGroup } from '@angular/forms';
 })
 export class NameeditorComponent implements OnInit {
   name = new FormControl('');
-
-  
+ 
   profileForm = new FormGroup({
     firstName: new FormControl(''),
     lastName: new FormControl(''),
+    addressgroup : new FormGroup({
+      addresslin1 : new FormControl(''),
+      addresslin2 : new FormControl(''),
+      street : new FormControl(''),
+      city: new FormControl(''),
+      zip: new FormControl(''),
+    })
   });
-  constructor() { }
+
+  // profileForm = this.fb.group({
+  //   firstName: ['', Validators.required],
+  //   lastName: [''],
+  //   address: this.fb.group({
+  //     street: [''],
+  //     city: [''],
+  //     state: [''],
+  //     zip: ['']
+  //   }),
+  // });
+
+
+  constructor(private fb: FormBuilder) { }
 
   ngOnInit(): void {
+    this.updateProfile();
   }
   updateName() {
     this.name.setValue('Nancy');
   }
   onSubmit() {
-    console.warn(this.profileForm.value);
+    console.log(this.profileForm.value);
   }
+  updateProfile() {
+    this.profileForm.patchValue({
+      firstName: 'Nancy',
+      addressgroup: {
+        addresslin1: 'Kothrud Pune 10',
+        street: '123 Drew Street'
+      }
+    });
+  }
+  
 }
